28f77e4b1df65a9c9f46e26f69945651b3bbfa86
Rod Stewart <rodstewart@gmail.com>
2024-11-09 05:31:27 -0600

    Starter Repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# gitops-2024-dm
+Derek Morgan and Andrew Brown GitOps 2024 Bootcamp
---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.69.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.5"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
